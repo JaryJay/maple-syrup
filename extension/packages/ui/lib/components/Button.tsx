06e436e6d2fa192d0f7b375417abb5c188a8e01e
Jay <jay.jrjren@gmail.com>
2024-12-26 15:57:07 -0500

    Add content button, update icon

--- extension/packages/ui/lib/components/Button.tsx
+++ extension/packages/ui/lib/components/Button.tsx
@@ -9,9 +9,9 @@ export function Button({ theme, className, children, ...props }: ButtonProps) {
   return (
     <button
       className={cn(
+        "bg-orange-500 hover:bg-orange-500/90 text-white px-2 py-1",
+        "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-4 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
         className,
-        'py-1 px-4 rounded shadow hover:scale-105',
-        theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
       )}
       {...props}>
       {children}
--- extension/pages/content-ui/src/App.tsx
+++ extension/pages/content-ui/src/App.tsx
@@ -11,13 +11,15 @@ export default function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
-      <div className="flex gap-1 text-blue-500">
-        Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
+    <div className="fixed right-0 top-24 mr-2 z-40">
+      <div className="relative">
+        <Button theme={theme} onClick={exampleThemeStorage.toggle} className='p-0 rounded-full hover:brightness-110 transition-all duration-200 shadow-md'>
+          <img src={chrome.runtime.getURL("content-ui/maple-syrup-logo.svg")} alt="Maple syrup logo" width={50} />
+        </Button>
+        <div className='absolute top-[-2px] left-[-2px] rounded-full shadow-sm bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center pointer-events-none'>
+          1
+        </div>
       </div>
-      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
-        Toggle Theme
-      </Button>
     </div>
   );
 }
